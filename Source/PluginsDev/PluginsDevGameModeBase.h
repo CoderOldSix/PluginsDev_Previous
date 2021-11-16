ed885f21ccec915c72dd97800506b17c365d4dfa
JackMaDrinkCoffee <596770421@qq.com>
2021-11-16 15:15:26 +0800

    +init操作！

---
+++ Source/PluginsDev/PluginsDevGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PluginsDevGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLUGINSDEV_API APluginsDevGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
